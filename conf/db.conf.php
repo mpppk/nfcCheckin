635a3e951e115ad5e576b8571f406835be7939e4
mpppk <niboshiporipori@gmail.com>
2014-05-10 17:30:28 +0900

    mysqlを利用する際のconfファイルを追加

---
+++ conf/db.conf.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'dsn' => 'mysql:host=localhost;dbname=nfcDB;charset=utf8',
+    'user' => 'nfcadmin',
+    'pass' => 'nfcadmin',
+);
+
+// define('DSN', 'mysql:host=localhost;dbname=nfcDB');
+// define('DB_USER', 'nfcadmin');
+// define('DB_PASSWORD', 'nfcadmin');
+
+
+?>

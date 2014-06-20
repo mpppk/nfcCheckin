a5588fa808586e938ae7479395697907c60a468b
mpppk <niboshiporipori@gmail.com>
2014-06-21 01:19:10 +0900

    add deviceボタンでタイムゲージが表示される

--- public/controller/DeviceController.js
+++ public/controller/DeviceController.js
@@ -14,10 +14,12 @@ $(function() {
 
     var deviceController = {
         __name: 'deviceController',
-
         deviceLogic: deviceLogic,
+        progressPer: 100,
+        intervalID: 0,
         __ready: function(){
             $(this.rootElement).prepend($("<h1>").text('your devices'));
+            this.$find('.progress').hide('slow');
             // this.load();
         },
         load: function(userID){
@@ -36,14 +38,41 @@ $(function() {
                     table.append($("<tr>"));
                     var tr = table.find($('tr'));
                     tr = tr.eq(i);
-                    console.log('tr num: ' + tr.length);
                     tr.append($("<td>").text(cardName));
                     tr.append($("<td>").text(data[i].idm_no));
                     tr.append($("<td>").text('test'));
                 }
             });
         },
+        '#addDeviceBtn click': function() {
+            var self = this;
+            clearInterval(self.intervalID);// ボタンを連続で押した場合に備える
+            this.progressPer = 100;
+            this.$find('.progress').show('slow');
+            this.$find('#progressMsg').empty();
+            this.$find('#progressMsg').append($('<div>').attr({class:'alert alert-info text-centor'})
+                .text('touch device to PaSoRi'));
+            this.intervalID = setInterval(function(){stepProgress();}, 100);
+            var stepProgress = function(){
+                // var self = this;
+                var progressBar = self.$find('.progress-bar');
+                progressBar.css('width', self.progressPer + '%');
+                var time = (self.progressPer/10).toFixed(1);
+                progressBar.text(time + 'sec');
+                self.progressPer -= 1;
+                if(self.progressPer <= 0){
+                    clearInterval(self.intervalID);
+                    self.$find('.progress').hide('slow');
+                    self.$find('#progressMsg').empty();
+                    self.$find('#progressMsg').append($('<div>').attr({class:'alert alert-danger text-centor'})
+                        .text('failed to find the card. please try again.'));
+                }
+            }
+        },
         hide: function(){
+            clearInterval(self.intervalID);// ボタンを連続で押した場合に備える
+            this.progressPer = 0;
+            this.$find('#progressMsg').empty();
             $(this.rootElement).hide('slow');
         },
         show: function(){
